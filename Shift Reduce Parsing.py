e71e8131dc914d4c91acb2fabec9347a43e88a0f
abinaya94200k-rgb <abinaya9.4.200k@gmail.com>
2026-04-13 21:32:18 +0530

    Refactor Shift Reduce Parsing into a class structure
    Refactored the Shift Reduce Parsing program to use a class structure, improving organization and readability. Added methods for reduction and parsing, and updated the main program to utilize the new class.

--- Shift Reduce Parsing.py
+++ Shift Reduce Parsing.py
@@ -1,67 +1,69 @@
-a = ""
-ac = ""
-stk = ""
-act = ""
-k = z = i = j = c = 0
+# Corrected Shift Reduce Parsing Program
+
+class ShiftReduceParser:
+    def __init__(self, grammar, input_string):
+        self.grammar = grammar
+        self.input = input_string + '$'
+        self.stack = []
+
+    # Try reduction
+    def reduce(self):
+        for lhs, productions in self.grammar.items():
+            for prod in productions:
+                prod_len = len(prod)
+
+                if prod_len <= len(self.stack):
+                    if ''.join(self.stack[-prod_len:]) == prod:
+                        # Perform reduction
+                        for _ in range(prod_len):
+                            self.stack.pop()
+                        self.stack.append(lhs)
+                        print(f"Reduce: {prod} -> {lhs}")
+                        return True
+        return False
+
+    # Parsing process
+    def parse(self):
+        print(f"{'Stack':<15} {'Input':<15} {'Action'}")
+
+        i = 0
+        start_symbol = list(self.grammar.keys())[0]
+
+        while True:
+            action_done = False
+
+            # Try reduction as much as possible
+            while self.reduce():
+                print(f"{''.join(self.stack):<15} {self.input[i:]:<15} Reduce")
+                action_done = True
+
+            # Accept condition
+            if ''.join(self.stack) == start_symbol and self.input[i:] == '$':
+                print(f"{''.join(self.stack):<15} {self.input[i:]:<15} Accept")
+                print("\nString Accepted ✅")
+                break
+
+            # Shift
+            if i < len(self.input):
+                self.stack.append(self.input[i])
+                print(f"{''.join(self.stack):<15} {self.input[i+1:]:<15} Shift")
+                i += 1
+                action_done = True
+
+            # If no action possible → reject
+            if not action_done:
+                print("\nString Rejected ❌")
+                break
 
 
-def check():
-    global stk, a, c, i, j
-    ac = "REDUCE TO E"
+# Main Program
+if __name__ == "__main__":
+    # Grammar: S → aSb | ab
+    grammar = {
+        "S": ["aSb", "ab"]
+    }
 
-    z = 0
-    while z < c:
-        # E -> id
-        if z + 1 < len(stk) and stk[z] == 'i' and stk[z + 1] == 'd':
-            stk = stk[:z] + 'E'
-            print(f"${stk}\t{a}$\t{ac}")
-            j += 1
+    input_string = "aabb"
 
-        # E -> E+E
-        elif z + 2 < len(stk) and stk[z] == 'E' and stk[z + 1] == '+' and stk[z + 2] == 'E':
-            stk = stk[:z] + 'E'
-            print(f"${stk}\t{a}$\t{ac}")
-            i -= 2
-
-        # E -> E*E
-        elif z + 2 < len(stk) and stk[z] == 'E' and stk[z + 1] == '*' and stk[z + 2] == 'E':
-            stk = stk[:z] + 'E'
-            print(f"${stk}\t{a}$\t{ac}")
-            i -= 2
-
-        # E -> (E)
-        elif z + 2 < len(stk) and stk[z] == '(' and stk[z + 1] == 'E' and stk[z + 2] == ')':
-            stk = stk[:z] + 'E'
-            print(f"${stk}\t{a}$\t{ac}")
-            i -= 2
-
-        z += 1
-
-
-# Main program
-print("GRAMMAR is E->E+E \n E->E*E \n E->(E) \n E->id")
-a = input("Enter input string: ")
-
-c = len(a)
-act = "SHIFT->"
-stk = ""
-
-print("stack \t input \t action")
-
-j = 0
-i = 0
-
-while j < c:
-    # Check for 'id'
-    if j + 1 < c and a[j] == 'i' and a[j + 1] == 'd':
-        stk += "id"
-        a = a[:j] + "  " + a[j+2:]
-        print(f"\n${stk}\t{a}$\t{act}id")
-        check()
-        j += 2
-    else:
-        stk += a[j]
-        a = a[:j] + " " + a[j+1:]
-        print(f"\n${stk}\t{a}$\t{act}symbol")
-        check()
-        j += 1
+    parser = ShiftReduceParser(grammar, input_string)
+    parser.parse()
